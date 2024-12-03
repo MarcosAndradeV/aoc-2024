b3ff5868d51046660d89838efcb0c5ad1ae40f62
MarcosAndradeV <mastermarcos1212@hotmail.com>
2024-12-03 18:21:37 -0300

    Day 02

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -3,12 +3,7 @@ use std::fs;
 const INPUT_PATH: &str = "tests/day_02/input.txt";
 const EXAMPLE_PATH: &str = "tests/day_02/example.txt";
 
-fn main(){
-    // part_1();
-    part_2();
-}
-
-fn part_1() {
+fn main() {
     let input: Vec<Vec<i32>> = fs::read_to_string(INPUT_PATH)
         .expect("no file error")
         .lines()
@@ -18,6 +13,22 @@ fn part_1() {
                 .collect()
         })
         .collect();
+    part_1(&input);
+    part_2(&input);
+    let input: Vec<Vec<i32>> = fs::read_to_string(EXAMPLE_PATH)
+        .expect("no file error")
+        .lines()
+        .map(|e| {
+            e.split_whitespace()
+                .map(|e| e.parse::<i32>().expect("no number error"))
+                .collect()
+        })
+        .collect();
+    part_1(&input);
+    part_2(&input);
+}
+
+fn part_1(input: &Vec<Vec<i32>>) {
     let mut safe_count = 0;
     for report in input {
         let diffs = report.windows(2).map(|elem| {
@@ -41,19 +52,29 @@ fn part_1() {
     println!("Part 1 - Result: {}", safe_count);
 }
 
-fn part_2() {
-    let input: Vec<Vec<i32>> = fs::read_to_string(EXAMPLE_PATH)
-        .expect("no file error")
-        .lines()
-        .map(|e| {
-            e.split_whitespace()
-                .map(|e| e.parse::<i32>().expect("no number error"))
-                .collect()
-        })
-        .collect();
-    let safe_count = 0;
-    for report in input {
-        dbg!(report);
+fn part_2(input: &Vec<Vec<i32>>) {
+    let mut safe_count = 0;
+    'report: for report in input {
+        for i in 0..report.len() {
+            let mut new_report = report.clone();
+            new_report.remove(i);
+            let diffs = new_report.windows(2).map(|elem| {
+                if elem.len() == 2 {
+                    elem[0] - elem[1]
+                } else {
+                    elem[0]
+                }
+            });
+            let all_inc_or_dec = diffs.clone().all(|e| e > 0) || diffs.clone().all(|e| e < 0);
+            let differ_by = diffs.clone().all(|e| {
+                let diff = e.abs();
+                1 <= diff && diff <= 3
+            });
+            if all_inc_or_dec && differ_by {
+                safe_count += 1;
+                continue 'report;
+            }
+        }
     }
 
     println!("Part 2 - Result: {}", safe_count);
